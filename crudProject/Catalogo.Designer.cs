3b478739b5646926979b75efdaa52f09958585fb
Renato <renato.rodrigues.si@hotmail.com>
2023-09-19 12:13:14 -0300

    Add project files.

---
+++ crudProject/Catalogo.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace crudProject
+{
+    partial class Catalogo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleNome = new System.Windows.Forms.Label();
+            this.titlePreco = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtPreco = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBuscarID = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ListProducts = new System.Windows.Forms.ListView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBuscarNome = new System.Windows.Forms.TextBox();
+            this.txtBuscarPreco = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titleNome
+            // 
+            this.titleNome.AutoSize = true;
+            this.titleNome.Location = new System.Drawing.Point(16, 66);
+            this.titleNome.Name = "titleNome";
+            this.titleNome.Size = new System.Drawing.Size(38, 13);
+            this.titleNome.TabIndex = 0;
+            this.titleNome.Text = "Nome:";
+            this.titleNome.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // titlePreco
+            // 
+            this.titlePreco.AutoSize = true;
+            this.titlePreco.Location = new System.Drawing.Point(16, 123);
+            this.titlePreco.Name = "titlePreco";
+            this.titlePreco.Size = new System.Drawing.Size(38, 13);
+            this.titlePreco.TabIndex = 1;
+            this.titlePreco.Text = "Preço:";
+            this.titlePreco.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(19, 82);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(173, 20);
+            this.txtNome.TabIndex = 3;
+            this.txtNome.TextChanged += new System.EventHandler(this.txtNome_TextChanged);
+            // 
+            // txtPreco
+            // 
+            this.txtPreco.Location = new System.Drawing.Point(19, 139);
+            this.txtPreco.Name = "txtPreco";
+            this.txtPreco.Size = new System.Drawing.Size(173, 20);
+            this.txtPreco.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(117, 188);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Salvar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label1.Location = new System.Drawing.Point(258, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Editar produto:";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // txtBuscarID
+            // 
+            this.txtBuscarID.Location = new System.Drawing.Point(261, 82);
+            this.txtBuscarID.Name = "txtBuscarID";
+            this.txtBuscarID.Size = new System.Drawing.Size(215, 20);
+            this.txtBuscarID.TabIndex = 9;
+            this.txtBuscarID.TextChanged += new System.EventHandler(this.txtBuscar_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(261, 235);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Atualizar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label2.Location = new System.Drawing.Point(16, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Adicionar produto:";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(258, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "ID:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // ListProducts
+            // 
+            this.ListProducts.HideSelection = false;
+            this.ListProducts.Location = new System.Drawing.Point(552, 66);
+            this.ListProducts.Name = "ListProducts";
+            this.ListProducts.Size = new System.Drawing.Size(218, 276);
+            this.ListProducts.TabIndex = 14;
+            this.ListProducts.UseCompatibleStateImageBehavior = false;
+            this.ListProducts.SelectedIndexChanged += new System.EventHandler(this.ListProducts_SelectedIndexChanged_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label4.Location = new System.Drawing.Point(549, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 17);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Produtos:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // txtBuscarNome
+            // 
+            this.txtBuscarNome.Location = new System.Drawing.Point(261, 139);
+            this.txtBuscarNome.Name = "txtBuscarNome";
+            this.txtBuscarNome.Size = new System.Drawing.Size(215, 20);
+            this.txtBuscarNome.TabIndex = 16;
+            // 
+            // txtBuscarPreco
+            // 
+            this.txtBuscarPreco.Location = new System.Drawing.Point(261, 188);
+            this.txtBuscarPreco.Name = "txtBuscarPreco";
+            this.txtBuscarPreco.Size = new System.Drawing.Size(215, 20);
+            this.txtBuscarPreco.TabIndex = 17;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(258, 123);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Nome:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(258, 172);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Preço:";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(401, 235);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 24;
+            this.button4.Text = "Deletar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Catalogo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(793, 356);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtBuscarPreco);
+            this.Controls.Add(this.txtBuscarNome);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ListProducts);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtBuscarID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPreco);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.titlePreco);
+            this.Controls.Add(this.titleNome);
+            this.Name = "Catalogo";
+            this.Text = "Catálogo de Produtos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleNome;
+        private System.Windows.Forms.Label titlePreco;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtPreco;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBuscarID;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListView ListProducts;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBuscarNome;
+        private System.Windows.Forms.TextBox txtBuscarPreco;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ crudProject/Catalogo.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace crudProject
+{
+    public partial class Catalogo : Form
+    {
+        private MySqlConnection conn;
+        private string data_source = "datasource=localhost;username=root;password=;database=db_products";
+        public Catalogo()
+        {
+            InitializeComponent();
+
+            ListProducts.View = View.Details;
+            ListProducts.LabelEdit = true;
+            ListProducts.AllowColumnReorder = true;
+            ListProducts.FullRowSelect = true;
+            ListProducts.GridLines = true;
+
+            ListProducts.Columns.Add("ID", 30, HorizontalAlignment.Left);
+            ListProducts.Columns.Add("Nome", 80, HorizontalAlignment.Left);
+            ListProducts.Columns.Add("Preço", 80, HorizontalAlignment.Left);
+
+            load_info();
+
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                // Criando conexão com o MySQL
+                conn = new MySqlConnection(data_source);
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand();
+
+                cmd.Connection = conn;
+
+                // Executando comando INSERT
+                cmd.CommandText = "INSERT INTO info (name, price) VALUES (@name, @price)";
+
+
+                cmd.Parameters.AddWithValue("@name", txtNome.Text);
+                cmd.Parameters.AddWithValue("@price", txtPreco.Text);
+
+                cmd.Prepare();
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Produto inserido", "Sucesso",
+                               MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+                //Deixando os campos vazios após a execução do comando
+                txtNome.Text = string.Empty;
+                txtPreco.Text = string.Empty;           
+                    
+                // Exibindo os produtos atualizados
+                load_info();
+
+                cmd.Parameters.Clear();
+
+
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Error " + ex.Number + " has ocurred: " + ex.Message,
+                                "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error has ocurred: " + ex.Message,
+                                "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                // Criando conexão com o MySQL
+                conn = new MySqlConnection(data_source);
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand();
+
+                cmd.Connection = conn;
+
+                // Executando comando UPDATE
+                cmd.CommandText = "UPDATE info SET name = @name, price = @price WHERE id = @id";
+
+                cmd.Parameters.AddWithValue("@name", txtBuscarNome.Text);
+                cmd.Parameters.AddWithValue("@price", txtBuscarPreco.Text);
+                cmd.Parameters.AddWithValue("@id", txtBuscarID.Text);
+
+                cmd.Prepare();
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Produto atualizado", "Sucesso",
+                               MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                
+                //Deixando os campos vazios após a execução do comando
+                txtBuscarID.Text = string.Empty;
+                txtBuscarNome.Text = string.Empty;
+                txtBuscarPreco.Text = string.Empty;
+
+                //Exibindo os produtos atualizados
+                load_info();
+                
+                cmd.Parameters.Clear();
+
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Error " + ex.Number + " has ocurred: " + ex.Message,
+                                "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error has ocurred: " + ex.Message,
+                                "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtBuscar_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listProducts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lst_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ListProducts_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                // Criando conexão com o MySQL
+                conn = new MySqlConnection(data_source);
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand();
+
+                cmd.Connection = conn;
+
+                //string sqlDelete = "DELETE FROM info WHERE id = '" + txtBuscarID.Text 
+
+                // Executando comando DELETE
+                cmd.CommandText = "DELETE FROM info WHERE id = @id";
+
+                cmd.Parameters.AddWithValue("@id", txtBuscarID.Text);
+
+                cmd.Prepare();
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Produto deletado", "Sucesso",
+                               MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+                //Deixando os campos vazios após a execução do comando
+                txtBuscarID.Text = string.Empty;
+                txtBuscarNome.Text = string.Empty;
+                txtBuscarPreco.Text = string.Empty;
+
+                //Exibindo os produtos atualizados
+                load_info();
+                
+                cmd.Parameters.Clear();
+
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Error " + ex.Number + " has ocurred: " + ex.Message,
+                                "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error has ocurred: " + ex.Message,
+                                "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+
+        private void txtNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void load_info()
+        {
+            try
+            {
+                // Criando conexão com o MySQL
+                conn = new MySqlConnection(data_source);
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand();
+
+                cmd.Connection = conn;
+
+                // Executando o comando SELECT 
+                cmd.CommandText = "SELECT * FROM info";
+
+                // Exibindo os dados coletados
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                ListProducts.Items.Clear();
+
+                while (reader.Read())
+                {
+                    string[] row =
+                    {
+                        reader.GetString(0),
+                        reader.GetString(1),
+                        reader.GetString(2),
+                    };
+
+                    ListProducts.Items.Add(new ListViewItem(row));
+
+                }
+
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Error " + ex.Number + " has ocurred: " + ex.Message,
+                                "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error has ocurred: " + ex.Message,
+                                "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            finally
+            {
+                conn.Close();
+
+            }
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
